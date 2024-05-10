1aaeb90e758dec465830961ee2f71d2de160f37f
AugustKim <August@hmm-dev-macbook.local>
2024-05-11 01:33:05 +0900

    splash를 scaffold로 구성

--- lib/app/src/screen/home/spalsh_screen.dart
+++ lib/app/src/screen/home/spalsh_screen.dart
@@ -12,16 +12,21 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Future.delayed(Duration(seconds: 5), () {
-      Navigator.of(context).popAndPushNamed('/home');
-    });
+    // Future.delayed(Duration(seconds: 5), () {
+    //   Navigator.of(context).popAndPushNamed('/home');
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Splash Screen'),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: const Text(
+            'Splash Screen',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+          ),
+        ),
       ),
     );
   }
