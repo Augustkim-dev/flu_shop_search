0b27516b2de3f5a80f8fe4ff0b01bcc6b5228f93
AugustKim <August@hmm-dev-macbook.local>
2024-05-15 01:46:20 +0900

    리스트뷰 디자인 작업 중

--- lib/app/src/screen/list/list_screen.dart
+++ lib/app/src/screen/list/list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -33,7 +35,36 @@ class _ListScreenState extends State<ListScreen> {
             child: ListView.builder(
                 itemCount: 10,
                 itemBuilder: (context, index) {
-                  return Text('$index');
+                  return Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              child: Image.network(
+                                  'https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20230516_56%2F1684202845985xPlS1_JPEG%2F2289333851333054_1602512952.JPG&type=sc960_832'),
+                              width: 72,
+                              height: 72,
+                            ),
+                            SizedBox(width: 20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Name of Shop',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 16)),
+                                Text('description of shop'),
+                                Text('Category'),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Divider(),
+                      ],
+                    ),
+                  );
                 }),
           ),
         ],
