20d90db5c554c00556e310cd4a153e3e7f544ac8
AugustKim <August@hmm-dev-macbook.local>
2024-05-26 22:25:55 +0900

    shop_model 추가 / fetchShopList 작성 중

---
+++ lib/app/src/model/shop_model.dart
@@ -0,0 +1,37 @@
+class ShopModel {
+  int? id;
+  String str_shop_name; // shop 이름
+  String str_phone;
+  String str_shop_url;
+  String str_desc;
+  String str_image_url;
+  bool? b_online;
+  bool? b_offline;
+  bool? b_domestic;
+  bool? b_international;
+  String str_address;
+  String str_lat;
+  String str_long;
+  // Map<String, dynamic> json_brand;
+  DateTime? created_at;
+
+  ShopModel({
+    this.id,
+    required this.str_shop_name,
+    required this.str_phone,
+    required this.str_shop_url,
+    required this.str_desc,
+    required this.str_image_url,
+    this.b_online,
+    this.b_offline,
+    this.b_domestic,
+    this.b_international,
+    required this.str_address,
+    required this.str_lat,
+    required this.str_long,
+    // required this.json_brand,
+    this.created_at,
+  });
+
+  // factory ShopModel.fromJson(Map<String, dynamic> json) {}
+}
